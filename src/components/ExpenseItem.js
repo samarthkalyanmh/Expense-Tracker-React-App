243c311b348e031053dfe53f9bb56c12e8ed7828
Samarth Kalyan M H <samarthkalyanmh777@gmail.com>
2024-01-25 14:16:38 +0530

    Made more changes added looping

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,21 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-const expenseDate = new Date(2021, 4, 4);
-console.log(expenseDate)
-const expenseTitle = 'Insurance'
-const expenseAmount = 298
-const location = 'CTA'
+function ExpenseItem(props) {
+
+  const month = props.date.toLocaleString('en-US', { month: 'long' })
+  const year = props.date.getFullYear()
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' })
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toLocaleDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}- {location}</h2>
-        <div className='expense-item__price'>${expenseAmount}</div>
+        <h2>{props.title}-{props.locationOfExpenditure}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   )
