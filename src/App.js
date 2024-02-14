7876fce40435d3b6cd554cc0cb2a5cc905eaccc3
Samarth Kalyan M H <samarthkalyanmh777@gmail.com>
2024-02-14 12:31:03 +0530

    Updating

--- src/App.js
+++ src/App.js
@@ -7,29 +7,35 @@ function App() {
       title: "Toilet Paper",
       amount: 94.12,
       date: new Date(2020, 7, 14),
-      locationOfExpenditure: 'CTA'
+      locationOfExpenditure: "CTA",
     },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e2",
+      title: "New TV",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+      locationOfExpenditure: "CTA",
+    },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
       date: new Date(2021, 2, 28),
-      locationOfExpenditure: 'CTA'
+      locationOfExpenditure: "CTA",
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
       date: new Date(2021, 5, 12),
-      locationOfExpenditure: 'CTA'
+      locationOfExpenditure: "CTA",
     },
   ];
 
   return (
     <div>
       <h1>SAM'S EXPENSE TRACKER APP</h1>
-      {expenses.map(({id, title, date, amount, locationOfExpenditure}) => (
+      {expenses.map(({ id, title, date, amount, locationOfExpenditure }) => (
         <ExpenseItem
           id={id}
           title={title}
